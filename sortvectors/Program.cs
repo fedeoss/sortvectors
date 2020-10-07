e3cc2f03682f0267a21bf3763438bcf816a46c6f
Federica Zani <FEDERICA.ZANI@local.ittcesena.it>
2020-10-07 11:07:09 +0200

    first step2
    s

---
+++ sortvectors/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace sortvectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vet = new int[10] { 4, 6, 7, 8, 9, 0, 1, 2, 1, 45 };
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(vet[i]);
+
+            Console.ReadKey();
+        }
+    }
+}
