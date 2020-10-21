77803fd6a96eac1be2f8f1ea725beb0c32ba7673
tommaso.zammarchi <tommaso.zammarchi@PC7324>
2020-10-21 11:24:35 +0200

    Ho aggiunto il metodo massimo

--- sortvectors/Program.cs
+++ sortvectors/Program.cs
@@ -7,10 +7,35 @@ namespace sortvectors
         static void Main(string[] args)
         {
             int[] vet = new int[10] { 4, 6, 7, 8, 9, 0, 1, 2, 1, 45 };
+
+            Ordina(vet);
+
+
+            int massimo = Massimo(vet);
+
+            Console.WriteLine("Il valore massimo è: " + massimo);
+
+
             for (int i = 0; i < 10; i++)
+            {
                 Console.WriteLine(vet[i]);
+            }
+                
+            
 
             Console.ReadKey();
+
+
         }
+        static void Ordina(int [] v)
+        {
+            Array.Sort(v);
+        }
+
+        static int Massimo(int[] v)
+        {
+            return v[9];
+        }
+
     }
 }
